87880ba2cbb3390ffee39ca4290f70ad4546412c
Aigars <aigarspeda@gmail.com>
2023-09-20 09:56:34 +0300

    updated index page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,44 +1,72 @@
-<script setup lang="ts"></script>
-
 <template>
   <main>
-    <!-- <h1>Start designing</h1> -->
-    <p>Siple and easy way to create</p>
-    <RouterLink class="link" to="/design">Start</RouterLink>
+    <div class="blob-container">
+      <Blob />
+      <div class="blob-content">
+        <RouterLink class="link" to="/design">START</RouterLink>
+      </div>
+    </div>
   </main>
 </template>
+
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+import Blob from "@/components/Blob.vue";
+</script>
 
 <style scoped>
 main {
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
   height: 100vh;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
 }
 
 p {
-  font-size: 1rem;
   font-weight: 700;
+  color: #f9fafb;
+  font-size: 1.2rem;
+}
+
+.blob-container {
+  position: relative;
+  background-color: #f8fafc;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.blob-content {
+  top: 50%;
+  left: 50%;
+  display: flex;
+  position: absolute;
+  text-align: center;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  transform: translate(-50%, -50%);
 }
 
 .link {
   margin-top: 1rem;
-  padding: 0.3rem 3rem;
-  border: 2px solid #000;
-  border-radius: 0.3rem;
+  font-weight: 600;
+  color: #ffffff;
+  font-size: 1.4rem;
+  letter-spacing: 0.1em;
   text-decoration: none;
-  font-weight: 500;
-  color: #000;
-  transition:
-    background-color 0.2s ease-in-out,
-    color 0.2s ease-in-out;
+  padding: 0.3rem 0.5rem;
+  transition: all 0.2s ease-in-out;
+  font-family: "Nunito", sans-serif;
+  border-bottom: 3px solid transparent;
 }
 
 @media (hover: hover) {
   .link:hover {
-    background-color: #000;
-    color: #fff;
+    border-bottom: 3px solid #f9fafb;
   }
 }
 </style>

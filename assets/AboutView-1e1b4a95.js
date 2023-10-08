dae7ead6a4fe64e4cfa386de3aa828125e368c74
Aigars <aigarspeda@gmail.com>
2023-10-08 08:21:47 +0300

    Updates

---
+++ assets/AboutView-1e1b4a95.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as s,a as o}from"./index-00226731.js";const c={},_={class:"about"},a=o("h1",null,"This is project is created to learn Vue.js and canvas.",-1),n=[a];function r(i,d){return t(),s("div",_,n)}const u=e(c,[["render",r]]);export{u as default};
